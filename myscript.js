91f76bd0037c605f296e646c745d86ee5f112f10
Samah Shahla <samahch6@gmail.com>
2022-09-23 16:49:19 +0300

    correcting remaining bugs

--- myscript.js
+++ myscript.js
@@ -1,10 +1,11 @@
-// used the methos of solving this by objects of classes
+// used the method of solving this by objects of classes
 // classes must be declared before using them
 // constructing functions for new defined classes
 class Board {
     constructor() {
         //every position is an inner div in the board, => 9 positions //this is an attribute of the class
         this.positions = Array.from(document.querySelectorAll('.column'));
+        this.remaining = this.positions;
         // giving the board a function to check who is the winner // this is a function of the class
         // combinations of numbers for knowing the winner will be based on the folloiwng order 
         // 0 1 2
@@ -45,7 +46,7 @@ class Player0 {
 
         this.takeTurn = function () {
             //adding an event listener on click for each inner div
-            board.positions.forEach(e => e.addEventListener('click', HandleTurnTaken) )
+            board.remaining.forEach(e => e.addEventListener('click', HandleTurnTaken) )
             
         };
 
@@ -53,7 +54,11 @@ class Player0 {
             // the target of the click event will have X as an inner text
             e.target.innerText = "X";
             // now remove the event listener because we should not be able to click when its not our turn
-            board.positions.forEach(e => e.removeEventListener('click', HandleTurnTaken) )
+            board.remaining.forEach(e => e.removeEventListener('click', HandleTurnTaken) )
+             //filtering the available positions on the board
+            board.remaining = board.positions.filter((p) => p.innerText == '');
+            console.log(board.remaining);
+            
         }
 
     }
@@ -62,23 +67,29 @@ class Player1 {
     constructor(board) {
         // filter the positions according to the innerText, if empty or not
         // availablePositions will be an array
-        let availablePositions = board.positions.filter((p) => p.innerText == '');
+        //let availablePositions = board.positions.filter((p) => p.innerText == '');
 
         this.takeTurn = function () {
 
-            availablePositions = board.positions.filter((p) => p.innerText == '');
-            console.log(availablePositions);
+            //availablePositions = board.positions.filter((p) => p.innerText == '');
+            //console.log(availablePositions);
             //adding an event listener on click for the unchosen divs
-            availablePositions.forEach(e => e.addEventListener('click', HandleTurnTaken) )
+            board.remaining.forEach(e => e.addEventListener('click', HandleTurnTaken) );
         };
 
         function HandleTurnTaken(e) {
             // the target of the click event will have X as an inner text
             e.target.innerText = "O";
-            availablePositions = board.positions.filter((p) => p.innerText == '');
-            console.log(availablePositions);
             // now remove the event listener because we should not be able to click when its not our turn
-            availablePositions.forEach(e => e.removeEventListener('click', HandleTurnTaken) )
+            board.remaining.forEach(e => e.removeEventListener('click', HandleTurnTaken) )
+            //filtering the available positions on the board
+            board.remaining = board.positions.filter((p) => p.innerText == '');
+            //console.log(board.remaining);
+            //availablePositions = board.positions.filter((p) => p.innerText == '');
+            //console.log(availablePositions);
+            
+            //availablePositions.forEach(e => e.removeEventListener('click', HandleTurnTaken) )
+            
         }
 
     }
@@ -99,7 +110,7 @@ class TictactoeGame {
             // keep track of changes being made to the DOM tree using an object of MutationObserver
             const observer = new MutationObserver(() => takeTurn());
             // to observe changes of each div, put observer on each position
-            board.positions.forEach((e) => observer.observe(e, config)); //obsrve each element based on the congig declared
+            board.positions.forEach((e) => observer.observe(e, config)); //obsrve each element based on the config declared
 
             //call takeTurn function to give turn to first playyer
             takeTurn();
